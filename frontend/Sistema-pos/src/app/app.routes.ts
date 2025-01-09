dacbc5280af53a90ec1dc9ab9d0fdef4c8ed40da
gabibalbuena <gabibalbuena123@gmail.com>
2025-01-09 14:17:04 -0300

    se modifico el navbar

--- frontend/Sistema-pos/src/app/app.routes.ts
+++ frontend/Sistema-pos/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ActualizarUsuarioComponent } from './componentes/usuarios/actualizar-usuario/actualizar-usuario.component';
 import { ListaUsuariosComponent } from './componentes/usuarios/lista-usuarios/lista-usuarios.component';
 import { RegistrarUsuarioComponent } from './componentes/usuarios/registrar-usuario/registrar-usuario.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { VentasComponent } from './componentes/ventas/ventas.component';
 export const routes: Routes = [
   { path: 'usuarios', component: ListaUsuariosComponent }, 
   { path: '', redirectTo: 'usuarios', pathMatch: 'full' },
-  { path: 'registrar-usuario',component : RegistrarUsuarioComponent},
-  { path: 'actualizar-usuario/:id',component : ActualizarUsuarioComponent}
+  { path: 'productos',component : ProductosComponent},
+  { path: 'ventas',component : VentasComponent}
 ];
